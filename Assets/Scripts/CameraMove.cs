7670c12618e01e1227616bbc878885b0483af3bc
Tudor Vaida <bassul@gmail.com>
2015-11-07 16:01:48 +0200

    Player moves instead of camera, morality sets beam of light alpha

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+	private void Update()
+	{
+
+	}
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+	private float mMorality=1f;
+	public float moralityMax=10f;
+	public float speedPerSecond=2f;
+	public bool stopped=false;
+	public MeshRenderer beam;
+
+	private void Update () {
+		if(stopped) {
+			return;
+		}
+		Vector3 newPos=transform.localPosition;
+		newPos.x+=speedPerSecond*Time.deltaTime;
+		transform.localPosition=newPos;
+	}
+	public float Morality {
+		get { return mMorality;}
+		set { 
+			mMorality=value;
+			Color color = beam.material.color;
+			color.a=mMorality/moralityMax;
+			beam.material.color=color;
+		}
+	}
+}
