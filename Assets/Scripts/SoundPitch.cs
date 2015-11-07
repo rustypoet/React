0beb37fd00d4d818a437e9b2c78f48a39ad443fc
Darian Velciov <falenone112@gmai.com>
2015-11-07 16:53:17 +0200

    no message

---
+++ Assets/Scripts/SoundPitch.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundPitch : MonoBehaviour
+{
+    public int startingPitch = 10;
+    public int timeToDecrease = 5;
+    AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.pitch = startingPitch;
+    }
+
+    void Update()
+    {
+        if (audio.pitch > 0)
+            audio.pitch -= Time.deltaTime * startingPitch / timeToDecrease;
+    }
+}
